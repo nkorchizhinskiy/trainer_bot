7caefef5b6023d7e838db45cd4ae9497242d4207
nkorchizhinskiy <korchizhinskiy@gmail.com>
2022-07-04 19:19:41 +0700

    Change Welcome message.

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,12 +1,16 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
 
-from tgbot.services.repository import Repo
+#from tgbot.services.repository import Repo
 
 
-async def user_start(m: Message, repo: Repo):
-    await repo.add_user(m.from_user.id)
-    await m.reply("Hello, user!")
+async def user_start(m: Message):
+#    await repo.add_user(m.from_user.id)
+    await m.reply("""
+            Привет, дорогой друг✊ \
+            \nЯ - твой личный помощник в мире спорта! \
+            \nВ управлении я прост, достаточно нажать на меню, которое ты уже использовал, скорее всего. \
+            \nВ нем выбери нужный пункт и погнали!🚀""")
 
 
 def register_user(dp: Dispatcher):
