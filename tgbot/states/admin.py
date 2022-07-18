bf469298b3ff888816f949e8d7257eb8439e6a23
nkorchizhinskiy <korchizhinskiy@gmail.com>
2022-07-18 18:04:26 +0700

    Add exercise's state into admin stategroup.

--- tgbot/states/admin.py
+++ tgbot/states/admin.py
@@ -1,6 +1,12 @@
 from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
+class AdminStates(StatesGroup):
+    """FSM for admin. Set states for extions."""
+    admin_menu = State()
+    exercise_menu = State()
+
+
 class AddExersice(StatesGroup):
     """States which work with adding exrcise into DB"""
     input_exercise_name = State()
@@ -8,19 +14,17 @@ class AddExersice(StatesGroup):
 
 
 class DeleteExersice(StatesGroup):
-    """ States which delete exercise from DB."""
+    """States which delete exercise from DB."""
     input_exersice_name = State()
 
 
 class ChangeExersice(StatesGroup):
-    """ States which delete exercise from DB."""
+    """States which delete exercise from DB."""
+    choice = State()
     input_exersice_name = State()
     input_exersice_description = State()
 
     
-class AdminStates(StatesGroup):
-    """ FSM for admin. Set states for extions."""
-    admin_menu = State()
 
 
     
