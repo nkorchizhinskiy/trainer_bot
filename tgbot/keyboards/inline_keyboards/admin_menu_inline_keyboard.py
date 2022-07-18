61625d429b4094c69a25608d1e2183c68847b5a5
nkorchizhinskiy <korchizhinskiy@gmail.com>
2022-07-18 18:03:20 +0700

    Add exercise's menu, change points in admin menu

--- tgbot/keyboards/inline_keyboards/admin_menu_inline_keyboard.py
+++ tgbot/keyboards/inline_keyboards/admin_menu_inline_keyboard.py
@@ -2,8 +2,18 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 
+
 admin_actions = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Добавить упражнение", callback_data="add_exercise")],
-    [InlineKeyboardButton(text="Удалить упражнение", callback_data="delete_exercise"), InlineKeyboardButton(text="Изменить упражнение", callback_data="change_exercise")]
+    [InlineKeyboardButton(text="Меню упражнений", callback_data="exercise_menu")]
+    ])
+
+exercise_actions = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Добавить", callback_data="add_exercise"), (InlineKeyboardButton(text="Просмотреть", callback_data="print_exercises"))],
+    [InlineKeyboardButton(text="Удалить", callback_data="delete_exercise"), InlineKeyboardButton(text="Изменить", callback_data="change_exercise")]
+    ])
+
+
+choice_of_change_exercise = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Изменить название", callback_data="change_exercise_name"), InlineKeyboardButton(text="Изменить описание", callback_data="change_exercise_description")],
     ])
     
